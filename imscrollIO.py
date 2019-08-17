b24d7820f5b6c64b69d3063fbbdf5c76c721032b
Tzu-Yu Lee <leejuyuu@gmail.com>
2019-08-18 00:35:41 +0800

    function: import intensity trafes

--- imscrollIO.py
+++ imscrollIO.py
@@ -16,12 +16,9 @@ def import_intensity_traces(datapath, filestr):
     intensity_file_path = datapath / (filestr + '_traces.dat')
     traces_file = sio.loadmat(intensity_file_path)
     arrayShape = traces_file['traces']['green'][0, 0].shape
-    a = (1, 100)
-    b = a + (120,)
-    t = arrayShape + (2 ,)
-    intensity = np.zeros((arrayShape + (2,)))
-    intensity[:, :, 0] = traces_file['traces']['green'][0, 0]
-    intensity[:, :, 1] = traces_file['traces']['red'][0, 0]
+    intensity = np.stack((traces_file['traces']['green'][0, 0],
+                                traces_file['traces']['red'][0, 0]),
+                               -1)
 
     intensity = xr.DataArray(intensity,
                              dims=('AOI', 'time', 'channel'),
