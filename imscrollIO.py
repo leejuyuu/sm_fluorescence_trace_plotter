448baa85c1e3c45182dbea89618a9caf17b280e4
Tzu-Yu Lee <leejuyuu@gmail.com>
2019-08-20 18:45:57 +0800

    imscrollIO: add function to import time stamps

--- imscrollIO.py
+++ imscrollIO.py
@@ -23,4 +23,32 @@ def import_intensity_traces(datapath, filestr):
                              dims=('AOI', 'time', 'channel'),
                              coords={'AOI': range(array_shape[0]),
                                      'channel': ['green', 'red']})
+    intensity.attrs['datapath'] = datapath
+    intensity.attrs['filestr'] = filestr
+
+    return intensity
+
+def import_image_path_from_driftfit(intensity):
+    driftfit_file_path = intensity.datapath / (intensity.filestr + '_driftfit.dat')
+    driftfit_file = sio.loadmat(driftfit_file_path)
+    intensity.attrs['image_path'] = Path(driftfit_file['aoifits']['tifFile'][0][0][0])
+    return intensity
+
+
+def import_time_stamps(intensity):
+    time_stamp_txt = intensity.attrs['image_path'].with_suffix('.txt')
+    time_data = np.loadtxt(time_stamp_txt)
+    time_data[1, :] = np.cumsum(time_data[1, :])
+    intcorrected_file_path = intensity.datapath / (intensity.filestr + '_intcorrected.dat')
+    intcorrected_file = sio.loadmat(intcorrected_file_path)
+    frames_used_in_intensity_cal = intcorrected_file['aoifits']['dataRed'][0, 0][:, 1]
+    frame_start = int(frames_used_in_intensity_cal.min())
+    frame_end = int(frames_used_in_intensity_cal.max())
+
+    intensity.attrs['frame_start'] = frame_start
+    intensity.attrs['frame_end'] = frame_end
+
+    time_stamps = time_data[1, frame_start:frame_end+1]
+    intensity = intensity.assign_coords(time=time_stamps)
+
     return intensity
