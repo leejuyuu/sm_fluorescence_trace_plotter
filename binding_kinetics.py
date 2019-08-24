2849d2d3139e8b89eabb996a60877c721f2eeb40
Tzu-Yu Lee <leejuyuu@gmail.com>
2019-08-24 21:55:45 +0800

    binding_kinetics: add functions to select good DNA tethers

--- binding_kinetics.py
+++ binding_kinetics.py
@@ -21,9 +21,39 @@ def remove_multiple_DNA(data, DNA_channel):
         return selected_state_label
 
 
-    def set_remove_more_than_two_states():
+    def get_number_of_states():
+        DNA_viterbi_state_label = data['viterbi_path'].sel(state='label', channel=DNA_channel)
+        nStates = DNA_viterbi_state_label.max(dim='time')
+        return nStates
 
-        return 0
-        
-    selected_state_label = find_state_with_lowest_intensity()
+
+    def remove_more_than_two_states():
+        badTethers = set(np.where(nStates>2)[0] + 1)
+        return badTethers
+
+
+    def remove_two_state_with_lowest_not_equal_to_zero(badTethers=set()):
+        good_tethers = all_tethers_set - badTethers
+        new_bad_tether_list = []
+        for iAOI in good_tethers:
+            if nStates.loc[iAOI] != 2:
+                continue
+            bool_lowest_state = data['viterbi_path'].sel(state='label', channel=DNA_channel, AOI=iAOI)\
+            ==lowest_state_label.loc[iAOI]
+
+            lowest_state_std = np.std(data['intensity'].sel(
+                                     channel=DNA_channel, AOI=iAOI)[bool_lowest_state])
+            lowest_state_mean = np.mean(data['viterbi_path'].sel(state='position',
+                                                                 channel=DNA_channel,
+                                                                 AOI=iAOI)[bool_lowest_state])
+            if abs(lowest_state_mean) > 2*lowest_state_std:
+                new_bad_tether_list.append(iAOI)
+        badTethers = badTethers | set(new_bad_tether_list)
+
+        return badTethers
+    lowest_state_label = find_state_with_lowest_intensity()
+    nStates = get_number_of_states()
+    all_tethers_set = set(data.AOI.values)
+    badTethers = remove_more_than_two_states()
+    remove_two_state_with_lowest_not_equal_to_zero(badTethers=badTethers)
     return 0
