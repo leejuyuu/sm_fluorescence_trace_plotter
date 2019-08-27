9c86821286545539958451054e879045c43b1cd2
Tzu-Yu Lee <leejuyuu@gmail.com>
2019-08-27 18:12:30 +0800

    binding_kinetics: add functions to find bad intervals without binding
    Also update old functions

--- binding_kinetics.py
+++ binding_kinetics.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def remove_multiple_DNA(data, DNA_channel):
@@ -28,28 +29,35 @@ def remove_multiple_DNA(data, DNA_channel):
         badTethers = set(np.where(nStates>2)[0] + 1)
         return badTethers
 
-    def remove_two_state_with_lowest_not_equal_to_zero(badTethers=set()):
-        good_tethers = all_tethers_set - badTethers
-        new_bad_tether_list = []
-        for iAOI in good_tethers:
-            if nStates.loc[iAOI] != 2:
-                continue
+    def check_if_lowest_state_equal_to_zero():
+        bool_lowest_state_equal_to_zero = xr.DataArray(np.zeros(len(data.AOI)),
+                                                       dims='AOI',
+                                                       coords={'AOI': data.AOI})
+        for iAOI in data.AOI:
             bool_lowest_state = data['viterbi_path'].sel(state='label', channel=DNA_channel, AOI=iAOI) \
                                 == lowest_state_label.loc[iAOI]
-
             lowest_state_std = np.std(data['intensity'].sel(
                 channel=DNA_channel, AOI=iAOI)[bool_lowest_state])
             lowest_state_mean = np.mean(data['viterbi_path'].sel(state='position',
                                                                  channel=DNA_channel,
                                                                  AOI=iAOI)[bool_lowest_state])
-            if abs(lowest_state_mean) > 2*lowest_state_std:
-                new_bad_tether_list.append(iAOI)
-        badTethers = badTethers | set(new_bad_tether_list)
+            bool_lowest_state_equal_to_zero.loc[iAOI] = abs(lowest_state_mean) < 2*lowest_state_std
+        return bool_lowest_state_equal_to_zero
 
+    def remove_two_state_with_lowest_not_equal_to_zero(badTethers=set()):
+        bad_tether_condition = (nStates != 2) \
+                               & xr.ufuncs.logical_not(bool_lowest_state_equal_to_zero)
+        new_bad_tether_set = set(data.AOI[bad_tether_condition].values)
+        badTethers = badTethers | new_bad_tether_set
         return badTethers
 
     lowest_state_label = find_state_with_lowest_intensity()
+    bool_lowest_state_equal_to_zero = check_if_lowest_state_equal_to_zero()
     nStates = get_number_of_states()
+    data['lowest_state_label'] = lowest_state_label
+    data['nStates'] = nStates
+    data['bool_lowest_state_equal_to_zero'] = bool_lowest_state_equal_to_zero
+
     all_tethers_set = set(data.AOI.values)
     badTethers = remove_more_than_two_states()
     badTethers = remove_two_state_with_lowest_not_equal_to_zero(badTethers=badTethers)
@@ -59,11 +67,17 @@ def remove_multiple_DNA(data, DNA_channel):
 
 
 def match_vit_path_to_intervals(data, DNA_channel):
-    state_sequence = data['viterbi_path'].sel(state='label', channel=DNA_channel, AOI=1)
-    state_start_index = find_state_end_point(state_sequence)
-    event_time = assign_event_time(state_sequence, state_start_index)
-    set_up_intervals(data.time, event_time)
-
+    bad_aoi_list = []
+    for iAOI in data.AOI:
+        print(iAOI)
+        state_sequence = data['viterbi_path'].sel(state='label', channel='green', AOI=iAOI)
+        state_start_index = find_state_end_point(state_sequence)
+        event_time = assign_event_time(state_sequence, state_start_index)
+        intervals = set_up_intervals(data.time, event_time)
+        intervals = assign_state_number_to_intervals(data.sel(AOI=iAOI), intervals)
+        if find_any_bad_intervals(data.sel(AOI=iAOI), intervals):
+            bad_aoi_list.append(iAOI.values)
+    print(bad_aoi_list)
     return 0
 
 
@@ -93,10 +107,51 @@ def set_up_intervals(time_coord, event_time):
     intervals['duration'] = xr.DataArray(np.diff(event_time),
                                          dims='interval_number',
                                          coords={'interval_number': intervals.interval_number})
-    for i in range(len(intervals.interval_number)):
-        intervals['indices'].loc[i] = tuple(time_coord[(event_time[i] <= time_coord)
-                                                       & (time_coord <= event_time[i+1])].values)
+    intervals['start'] = xr.DataArray(event_time[0:-1], dims='interval_number')
+    intervals['end'] = xr.DataArray(event_time[1:], dims='interval_number')
     return intervals
+
+
+def shift_state_number(AOI_data):
+    if AOI_data['bool_lowest_state_equal_to_zero']:
+        if AOI_data['lowest_state_label'] == 1:
+            AOI_data['viterbi_path'].loc[:, 'label', 'green'] = AOI_data['viterbi_path'].loc[:, 'label', 'green']-1
+        else:
+            raise ValueError('shift_state_number:\nlowest state not equal to 1')
+    return AOI_data
+
+
+
+def assign_state_number_to_intervals(AOI_data, intervals):
+    AOI_data = shift_state_number(AOI_data)
+    intervals_state_number = xr.DataArray(np.zeros(len(intervals.interval_number)),
+                                          dims='interval_number',
+                                          coords={'interval_number': intervals.interval_number})
+    for i in intervals['interval_number']:
+        interval_slice = slice(intervals['start'].loc[i], intervals['end'].loc[i])
+        distinct_state_numbers = set(AOI_data['viterbi_path'].sel(state='label',
+                                                                  channel='green',
+                                                                  time=interval_slice).values)
+        if len(distinct_state_numbers) == 1:
+            intervals_state_number.loc[i] = list(distinct_state_numbers)[0]
+
+        else:
+            raise ValueError('assign_state_number_to_intervals:\nThere are more than one state in this interval.')
+    intervals['state_number'] = intervals_state_number
+    return intervals
+
+
+def find_any_bad_intervals(AOI_data, intervals):
+    out = False
+    for i in intervals['interval_number']:
+        interval_slice = slice(intervals['start'].loc[i], intervals['end'].loc[i])
+        chunk_of_interval_traces = AOI_data['interval_traces'].sel(time=interval_slice,
+                                                                   channel='green')
+        if (intervals['state_number'].loc[i] != 0) & \
+                (sum(chunk_of_interval_traces) < 0.9*len(chunk_of_interval_traces)):
+            out = True
+            break
+    return out
 
 
 
