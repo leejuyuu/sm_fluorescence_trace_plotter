86d2d8a9ea208bfcf4bfe5b86620d86a67d875ca
Tzu-Yu Lee <leejuyuu@gmail.com>
2019-10-16 22:44:20 +0800

    refactoring

--- 0911analysis_part2.py
+++ 0911analysis_part2.py
@@ -2,97 +2,47 @@ from pathlib import Path
 import pandas as pd
 import imscrollIO
 import binding_kinetics as bk
-import binding_kinetics
-xlspath = Path('D:/TYL/PriA_project/Analysis_Results/20190917/20190917parameterFile.xlsx')
-# xlspath = Path('/mnt/linuxData/Research/PriA_project/analysis_result/20190911/20190911parameterFile.xlsx')
 
-datapath = imscrollIO.def_data_path()
-# datapath = Path('/mnt/linuxData/Research/PriA_project/analysis_result/20190911/20190911imscroll')
-sheet_list = ['L1', 'L2', 'L3', 'L4']
-for i_sheet in sheet_list:
-    dfs = pd.read_excel(xlspath, sheet_name=i_sheet)
-    nFiles = dfs.shape[0]
-    for iFile in range(0, nFiles):
-        filestr = dfs.filename[iFile]
-        AOI_categories = {}
 
-        try:
-            data = imscrollIO.load_data_from_json(datapath / (filestr + '_data.json'))
-        except FileNotFoundError:
-            continue
+def categorize_binding_traces():
+    # xlspath = Path('D:/TYL/PriA_project/Analysis_Results/20190917/20190917parameterFile.xlsx')
+    xlspath = Path('/mnt/linuxData/Research/PriA_project/analysis_result/20190911/20190911parameterFile.xlsx')
 
-        state_info = bk.collect_all_channel_state_info(data)
-        DNA_channel_data = bk.get_channel_data(data, data.target_channel)
-        bad_tethers = bk.list_multiple_DNA(state_info.sel(channel=data.target_channel))
-        bad_tethers_list = list(bad_tethers)
-        bad_tethers_list.sort()
-        AOI_categories['multiple_tethers'] = bad_tethers_list
-        selected_data \
-            = bk.split_data_set_by_specifying_aoi_subset(data, bad_tethers)[1]
-        selected_state_info = \
-            bk.split_data_set_by_specifying_aoi_subset(state_info, bad_tethers)[1]
+    # datapath = imscrollIO.def_data_path()
+    datapath = Path('/mnt/linuxData/Research/PriA_project/analysis_result/20190911/20190911imscroll')
+    SHEET_LIST = ['L1']
+    for i_sheet in SHEET_LIST:
+        dfs = pd.read_excel(xlspath, sheet_name=i_sheet)
+        nFiles = dfs.shape[0]
+        for iFile in range(0, nFiles):
+            filestr = dfs.filename[iFile]
+            AOI_categories = {}
 
-        protein_channel_data = bk.get_channel_data(selected_data, data.binder_channel[0])
-        bad_aoi_set, intervals = bk.match_vit_path_to_intervals(
-            protein_channel_data)
-        bad_aoi_list = list(bad_aoi_set)
-        bad_aoi_list.sort()
+            try:
+                data = imscrollIO.load_data_from_json(datapath / (filestr + '_data.json'))
+            except FileNotFoundError:
+                continue
 
-        AOI_categories['false_binding'] = bad_aoi_list
-        selected_data \
-            = bk.split_data_set_by_specifying_aoi_subset(selected_data, bad_aoi_set)[1]
-        good_intervals = \
-            bk.split_data_set_by_specifying_aoi_subset(intervals, bad_aoi_set)[1]
+            state_info = bk.collect_all_channel_state_info(data)
+            bad_tethers = bk.list_multiple_tethers(state_info.sel(channel=data.target_channel))
+            AOI_categories['multiple_tethers'] = bad_tethers
+            remaining_aois = list(set(data.AOI.values) - set(bad_tethers))
+            selected_data = data.sel(AOI=remaining_aois)
+            protein_channel_data = bk.get_channel_data(selected_data, data.binder_channel[0])
+            non_colocalized_aois, intervals = bk.colocalization_analysis(protein_channel_data)
+            AOI_categories['false_binding'] = non_colocalized_aois
+            analyzable_aois = list(set(remaining_aois) - set(non_colocalized_aois))
+            good_intervals = intervals.sel(AOI=analyzable_aois)
+            AOI_categories['analyzable'] = \
+                bk.group_analyzable_aois_into_state_number(good_intervals)
+            all_data = {'data': data,
+                        'intervals': intervals,
+                        'state_info': state_info}
+            bk.save_all_data(all_data, AOI_categories, datapath / (filestr + '_all.json'))
 
-        AOI_categories['analyzable'] = \
-            bk.group_analyzable_aois_into_state_number(selected_data, good_intervals)
-        all_data = {'data': data,
-                    'intervals': intervals,
-                    'state_info': state_info}
-        bk.save_all_data(all_data, AOI_categories, datapath / (filestr + '_all.json'))
+            print(filestr + ' finished')
+    return None
 
-        print(filestr + ' finished')
-        123
-    
-    #
-    # nAOIs = len(data.AOI)
-    #
-    #
-    # if not os.path.isdir(datapath / filestr):
-    #     os.mkdir(datapath / filestr)
-    # for iAOI in data.AOI:
-    #
-    #     plt.figure(figsize=(10, 5))
-    #
-    #     plt.suptitle('molecule {}'.format(iAOI.values), fontsize=14)
-    #     plt.xlim((0, data.time.max()))
-    #     plt.xlabel('time', fontsize=16)
-    #
-    #     plt.ylabel('Intensity', fontsize=16)
-    #     for i, i_channel in enumerate(data.channel.values, 1):
-    #
-    #         plt.subplot(3, 1, i)
-    #         y = data['intensity'].sel(AOI=iAOI, channel=i_channel)
-    #         bool_nan = xr.ufuncs.logical_not(xr.ufuncs.isnan(y))
-    #         plt.plot(data.time.where(bool_nan, drop=True),
-    #                  y.where(bool_nan, drop=True),
-    #                  color=i_channel
-    #                  )
-    #         plt.xlim((0, data.time.max()))
-    #
-    #
-    #
-    #     plt.xlabel('time (s)', fontsize=16)
-    #
-    #     plt.ylabel('Intensity', fontsize=16)
-    #     # plt.show()
-    #
-    #
-    #
-    #
-    #     plt.savefig(datapath / filestr / ('molecule{}.png'.format(iAOI.values)), Transparent=True,
-    #                 dpi=300, bbox_inches='tight')
-    #
-    #     plt.close()
 
-123
+if __name__ == '__main__':
+    categorize_binding_traces()
--- binding_kinetics.py
+++ binding_kinetics.py
@@ -4,27 +4,28 @@ import json
 from collections import namedtuple
 
 
-def find_state_intensity_and_std(channel_data):
-    number_of_states_array = get_number_of_states(channel_data)
+def find_state_intensity_and_std(channel_time_series):
+    """Return intensity mean and standard deviation in namedtuple, nested in AOI > states order"""
+    number_of_states_array = get_number_of_states(channel_time_series)
     mean_std_pair = namedtuple('mean_std_pair', ('mean', 'std'))
-    intensity_list = []
-    for i_aoi in channel_data.AOI:
+    intensity_pairs = []
+    for i_aoi in channel_time_series.AOI:
         number_of_states = number_of_states_array.sel(AOI=i_aoi)
-        viterbi_intensity = channel_data['viterbi_path'].sel(state='position', AOI=i_aoi)
-        viterbi_state_labels = channel_data['viterbi_path'].sel(state='label', AOI=i_aoi)
-        raw_intensity_array = channel_data['intensity'].sel(AOI=i_aoi)
-        pair_list = []
+        viterbi_intensity = channel_time_series['viterbi_path'].sel(state='position', AOI=i_aoi)
+        viterbi_state_labels = channel_time_series['viterbi_path'].sel(state='label', AOI=i_aoi)
+        raw_intensity = channel_time_series['intensity'].sel(AOI=i_aoi)
+        i_aoi_pairs = []
         for state_label in range(1, int(number_of_states) + 1):
-            state_intensity_array = viterbi_intensity[viterbi_state_labels == state_label]
-            unique_intensity = np.unique(state_intensity_array)
-            if len(unique_intensity) != 1:
+            state_viterbi_intensity = viterbi_intensity[viterbi_state_labels == state_label]
+            state_unique_intensity = np.unique(state_viterbi_intensity)
+            if len(state_unique_intensity) != 1:
                 raise ValueError('State intensity is not unique.')
-            state_raw_intensity = raw_intensity_array[viterbi_state_labels == state_label]
+            state_raw_intensity = raw_intensity[viterbi_state_labels == state_label]
             intensity_std = np.std(state_raw_intensity)
-            pair_list.append(mean_std_pair(unique_intensity.item(), intensity_std.item()))
-        intensity_list.append(tuple(pair_list))
+            i_aoi_pairs.append(mean_std_pair(state_unique_intensity.item(), intensity_std.item()))
+        intensity_pairs.append(tuple(i_aoi_pairs))
 
-    return intensity_list
+    return intensity_pairs
 
 
 def find_state_with_lowest_intensity(state_intensity_pairs):
@@ -41,38 +42,38 @@ def check_if_state_equal_to_zero(state_intensity):
 
 
 def get_number_of_states(channel_data):
-    DNA_viterbi_state_label = channel_data['viterbi_path'].sel(state='label')
-    nStates = DNA_viterbi_state_label.max(dim='time')
+    viterbi_state_labels = channel_data['viterbi_path'].sel(state='label')
+    number_of_states = viterbi_state_labels.max(dim='time')
 
-    return nStates
+    return number_of_states
 
 
-def remove_more_than_two_states(nStates):
+def list_aois_more_than_two_states(nStates):
+    """Returns a list containing aois that visits more than 2 states."""
     # np returns zero index so plus 1
-    badTethers = set((np.where(nStates > 2)[0] + 1).tolist())
-    return badTethers
+    aoi_list = (np.where(nStates > 2)[0] + 1).tolist()
+    return aoi_list
 
 
-def remove_two_state_with_lowest_not_equal_to_zero(channel_state_info, bad_tethers=None):
-    if bad_tethers is None:
-        bad_tethers = set()
-    bad_tether_condition = ((channel_state_info.nStates == 2)
-                           & np.logical_not(channel_state_info.bool_lowest_state_equal_to_zero))
-    new_bad_tether_set = set(channel_state_info.AOI[bad_tether_condition].values.tolist())
-    bad_tethers = bad_tethers | new_bad_tether_set
-    return bad_tethers
+def list_aois_elevated_two_states(channel_state_info):
+    """Returns a list containing aois that visits only 2 states
+    but the lowest state is not at baseline 0, which indicates multiple tether."""
+    condition = ((channel_state_info.nStates == 2)
+                 & np.logical_not(channel_state_info.bool_lowest_state_equal_to_zero))
+    aoi_list = channel_state_info.AOI[condition].values.tolist()
+    return aoi_list
 
 
-def collect_channel_state_info(channel_data):
-    intensity_pair_list = find_state_intensity_and_std(channel_data)
+def collect_channel_state_info(channel_time_series):
+    intensity_pairs = find_state_intensity_and_std(channel_time_series)
     lowest_state_label = []
     is_lowest_state_equal_to_zero_list = []
-    for state_intensity_pair in intensity_pair_list:
-        lowest_state_index = find_state_with_lowest_intensity(state_intensity_pair)
-        is_lowest_state_equal_zero = check_if_state_equal_to_zero(state_intensity_pair[lowest_state_index])
+    for i_aoi_pairs in intensity_pairs:
+        lowest_state_index = find_state_with_lowest_intensity(i_aoi_pairs)
+        is_lowest_state_equal_zero = check_if_state_equal_to_zero(i_aoi_pairs[lowest_state_index])
         lowest_state_label.append(lowest_state_index + 1)
         is_lowest_state_equal_to_zero_list.append(is_lowest_state_equal_zero)
-    nStates = get_number_of_states(channel_data)
+    nStates = get_number_of_states(channel_time_series)
     channel_state_info = xr.Dataset({'nStates': (['AOI'], nStates),
                                      'lowest_state_label': (['AOI'], lowest_state_label),
                                      'bool_lowest_state_equal_to_zero': (['AOI'], is_lowest_state_equal_to_zero_list)},
@@ -92,13 +93,16 @@ def collect_all_channel_state_info(data):
     return channel_state_info
 
 
-def list_multiple_DNA(channel_state_info):
-    bad_tethers = remove_more_than_two_states(channel_state_info.nStates)
-    bad_tethers = remove_two_state_with_lowest_not_equal_to_zero(channel_state_info, bad_tethers=bad_tethers)
+def list_multiple_tethers(channel_state_info) -> tuple:
+    bad_tethers1 = list_aois_more_than_two_states(channel_state_info.nStates)
+    bad_tethers2 = list_aois_elevated_two_states(channel_state_info)
+    bad_tethers = list(set(bad_tethers1) | set(bad_tethers2))
+    bad_tethers.sort()
+    bad_tethers = tuple(bad_tethers)
     return bad_tethers
 
 
-def split_data_set_by_specifying_aoi_subset(data, aoi_subset):
+def split_data_set_by_specifying_aoi_subset(data, aoi_subset: set):
     whole_aoi_set = set(data.AOI.values)
     complement_subset = whole_aoi_set - aoi_subset
     data_subset = data.sel(AOI=list(aoi_subset))
@@ -106,7 +110,7 @@ def split_data_set_by_specifying_aoi_subset(data, aoi_subset):
     return data_subset, data_complement
 
 
-def match_vit_path_to_intervals(channel_data):
+def colocalization_analysis(channel_data):
     channel_state_info = collect_channel_state_info(channel_data)
     channel_data = channel_data.merge(channel_state_info)
     bad_aoi_list = []
@@ -121,13 +125,13 @@ def match_vit_path_to_intervals(channel_data):
         intervals_list.append(i_intervals)
         if find_any_bad_intervals(channel_data.sel(AOI=iAOI), i_intervals):
             bad_aoi_list.append(int(iAOI.values))
-    print(bad_aoi_list)
-    bad_aoi_set = set(bad_aoi_list)
+    bad_aoi_list.sort()
+    bad_aois = tuple(bad_aoi_list)
     intervals = xr.concat(intervals_list, dim='AOI')
     intervals = intervals.assign_coords(AOI=channel_data.AOI)
     intervals = intervals.assign_coords(channel=channel_data.channel.item())
 
-    return bad_aoi_set, intervals
+    return bad_aois, intervals
 
 
 def find_state_end_point(state_sequence):
@@ -164,8 +168,7 @@ def set_up_intervals(event_time):
 def shift_state_number(AOI_data):
     if AOI_data['bool_lowest_state_equal_to_zero']:
         if AOI_data['lowest_state_label'] == 1:
-            AOI_data['viterbi_path'].loc[dict(state='label')] = \
-                AOI_data['viterbi_path'].loc[dict(state='label')] - 1
+            AOI_data['viterbi_path'].loc[dict(state='label')] += -1
         else:
             raise ValueError('shift_state_number:\nlowest state not equal to 1')
     return AOI_data
@@ -184,39 +187,38 @@ def assign_state_number_to_intervals(AOI_data, intervals):
             intervals_state_number.loc[i] = list(distinct_state_numbers)[0]
 
         else:
-            raise ValueError('assign_state_number_to_intervals:\nThere are more than one state in this interval.')
+            raise ValueError("""assign_state_number_to_intervals:
+            There are more than one state in this interval.""")
     intervals['state_number'] = intervals_state_number
     return intervals
 
 
 def find_any_bad_intervals(AOI_data, intervals):
-    out = False
+    any_bad_interval = False
     for i in intervals['interval_number']:
         interval_slice = slice(intervals['start'].loc[i], intervals['end'].loc[i])
         chunk_of_interval_traces = AOI_data['interval_traces'].sel(time=interval_slice)
-        if (intervals['state_number'].loc[i] != 0) and \
-                (np.count_nonzero(chunk_of_interval_traces.values % 2 != 0)
-                 < 0.9 * len(chunk_of_interval_traces)):
-            out = True
+        # interval trace: (-2, 0, 2)=on, (-1,1, 3)=off
+        num_of_colocalized_time_points = np.count_nonzero(chunk_of_interval_traces.values % 2 != 0)
+
+        # Viterbi on state must correspond to colocalized event, with 10% tolerance
+        reject_condition_1 = intervals['state_number'].loc[i] != 0
+        reject_condition_2 = num_of_colocalized_time_points < 0.9 * len(chunk_of_interval_traces)
+        if reject_condition_1 and reject_condition_2:
+            any_bad_interval = True
             break
-    return out
+    return any_bad_interval
 
 
-def group_analyzable_aois_into_state_number(data, intervals):
-    # plus zero (baseline)
-    data['nStates'] = get_number_of_states(data.sel(channel=intervals.channel.item()))
-
-
+def group_analyzable_aois_into_state_number(intervals):
     analyzable_tethers = {}
-
     for i_state in range(int(intervals['state_number'].max()) + 1):
         i_index = intervals['state_number'].max(dim='interval_number') == i_state
         analyzable_tethers[i_state] = i_index.AOI.where(i_index, drop=True).values.tolist()
-
     return analyzable_tethers
 
 
-def extract_dwell_time(intervals_list, selection, state):
+def extract_dwell_time(intervals_list, state):
     # selection = 1 for end, -1 for start, 0 for intermediate
     out_list = []
     for i_file_intervals in intervals_list:
