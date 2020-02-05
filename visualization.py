705df5dbc02d70b2ae16dd0461458391e4bb9b17
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-02-05 21:39:07 +0800

    visualization: Group the plotting parts into function
    Make the plotting core into a function plot_one_trace_and_save(), thus callable
    from outside.

--- visualization.py
+++ visualization.py
@@ -4,11 +4,37 @@ from pathlib import Path
 import pandas as pd
 import imscrollIO
 import binding_kinetics
+import xarray as xr
+
+
+def plot_one_trace_and_save(molecule_data: xr.Dataset, category: str = '',
+                            save_dir: Path = Path(), save_format: str = 'svg'):
+    molecule_number = int(molecule_data.AOI)
+    fig = plt.figure(figsize=(10, 5))
+    plt.suptitle('molecule {} ({})'.format(molecule_number, category), fontsize=14)
+    channel_list = list(set(molecule_data.channel.values.tolist()))
+    channel_list.sort()
+    for i, i_channel in enumerate(channel_list, 1):
+        plt.subplot(len(channel_list), 1, i)
+        y = molecule_data['intensity'].sel(channel=i_channel)
+        vit = molecule_data['viterbi_path'].sel(channel=i_channel, state='position')
+        plt.plot(y.time, y, color=i_channel)
+        plt.plot(vit.time, vit, color='black', linewidth=2)
+        if plt.ylim()[0] > 0:
+            plt.ylim(bottom=0)
+        plt.xlim((0, molecule_data.time.max()))
+
+    fig.text(0.04, 0.4, 'Intensity', ha='center', fontsize=16, rotation='vertical')
+    plt.xlabel('time (s)', fontsize=16)
+    plt.savefig(save_dir / ('molecule{}.{}'.format(molecule_number, save_format)),
+                Transparent=True, dpi=300, bbox_inches='tight', format=save_format)
+    plt.close()
+    return None
 
 
 def main():
-    xlspath = Path('D:/TYL/PriA_project/Analysis_Results/20190917/20190917parameterFile.xlsx')
-    sheet = 'L3'
+    xlspath = Path('/run/media/tzu-yu/linuxData/Research/PriA_project/analysis_result/20191106//20191106parameterFile.xlsx')
+    sheet = 'L1_03'
     dfs = pd.read_excel(xlspath, sheet_name=sheet)
     datapath = imscrollIO.def_data_path()
     im_format = 'svg'
@@ -34,40 +60,10 @@ def main():
                     AOI_dict[key2] = value2
 
         for key, AOI_list in AOI_dict.items():
-
             for iAOI in AOI_list:
-
-                iAOI = int(iAOI)
-                fig = plt.figure(figsize=(10, 5))
-
-                plt.suptitle('molecule {} ({})'.format(iAOI, key), fontsize=14)
-
-                plt.xlim((0, data.time.max()))
-                plt.xlabel('time', fontsize=16)
-                # plt.ylabel('Intensity', fontsize=16)
-                channel_list = list(set(data.channel.values.tolist()))
-                channel_list.sort()
-                for i, i_channel in enumerate(channel_list, 1):
-
-                    plt.subplot(len(channel_list), 1, i)
-                    y = data['intensity'].sel(AOI=iAOI, channel=i_channel)
-                    vit = data['viterbi_path'].sel(AOI=iAOI, channel=i_channel, state='position')
-                    plt.plot(y.time, y,
-                             color=i_channel
-                             )
-                    plt.plot(vit.time, vit, color='black', linewidth=2)
-                    if plt.ylim()[0] > 0:
-                        plt.ylim(bottom=0)
-                    plt.xlim((0, data.time.max()))
-
-                fig.text(0.04, 0.4, 'Intensity', ha='center', fontsize=16, rotation='vertical')
-                plt.xlabel('time (s)', fontsize=16)
-                # plt.show()
-
-                plt.savefig(datapath / filestr / ('molecule{}.'.format(iAOI)+im_format), Transparent=True,
-                            dpi=300, bbox_inches='tight', format=im_format)
-
-                plt.close()
+                molecule_data = data.sel(AOI=iAOI)
+                plot_one_trace_and_save(molecule_data, key,
+                                        save_dir=(datapath / filestr), save_format=im_format)
     return None
 
 
