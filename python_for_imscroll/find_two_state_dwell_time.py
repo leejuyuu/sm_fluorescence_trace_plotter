0558506fc695a626affae0c2c8d8c93ee4ea2ef9
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-02-09 20:46:41 +0800

    Wrap the plotting part into function
    Also clean up some other parts.

--- python_for_imscroll/find_two_state_dwell_time.py
+++ python_for_imscroll/find_two_state_dwell_time.py
@@ -1,4 +1,23 @@
-from typing import List
+#  Copyright (C) 2020 Tzu-Yu Lee, National Taiwan University
+#
+#  This file (find_two_state_dwell_time.py) is part of python_for_imscroll.
+#
+#  python_for_imscroll is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  python_for_imscroll is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with python_for_imscroll.  If not, see <https://www.gnu.org/licenses/>.
+
+"""This is a temporary script that analyzes the dwell times of two state model."""
+
+from typing import List, Tuple
 from pathlib import Path
 import numpy as np
 import pandas as pd
@@ -10,10 +29,9 @@ from python_for_imscroll import binding_kinetics
 
 def find_two_state_dwell_time(parameter_file_path: Path, sheet_list: List[str]):
     datapath = imscrollIO.def_data_path()
-    specified_n_state = 1
+    state_category = '1'
     state_list = ['low', 'high']
-    on_off_str = ['on', 'off']
-    obs_off_str = ['obs', 'off']
+
     im_format = 'svg'
     for i_sheet in sheet_list:
         dfs = pd.read_excel(parameter_file_path, sheet_name=i_sheet)
@@ -30,10 +48,10 @@ def find_two_state_dwell_time(parameter_file_path: Path, sheet_list: List[str]):
                 continue
 
             print(filestr + ' loaded')
-            if str(specified_n_state) in AOI_categories['analyzable']:
-
-                interval_list.append(all_data['intervals'].sel(AOI=AOI_categories['analyzable'][str(specified_n_state)]))
-            n_good_traces += len(AOI_categories['analyzable']['1'])
+            if state_category in AOI_categories['analyzable']:
+                aoi_list = AOI_categories['analyzable'][state_category]
+                n_good_traces += len(aoi_list)
+                interval_list.append(all_data['intervals'].sel(AOI=aoi_list))
         max_time = all_data['data'].time.values.max()
         for i, item in enumerate(state_list):
             dwells = binding_kinetics.extract_dwell_time(interval_list, i)
@@ -44,28 +62,39 @@ def find_two_state_dwell_time(parameter_file_path: Path, sheet_list: List[str]):
             exf = ExponentialFitter()
             kmf.fit(dwells.duration, dwells.event_observed)
             exf.fit(dwells.duration, dwells.event_observed)
-
-            ax = kmf.plot()
-            exf.plot_survival_function(ax=ax, ci_show=False)
-            ax.get_legend().remove()
-
-            plt.xlabel(r'$\tau_{{{}}}$ (s)'.format(on_off_str[i]), fontsize=16)
-            plt.ylabel('probability', fontsize=16)
-            k_str = r'$k_{{{}}}$ = {:.1f} s'.format(obs_off_str[i], exf.lambda_)
-
             n_event = np.count_nonzero(dwells.event_observed)
             n_censored = len(dwells.event_observed) - n_event
-            string = '{}, {}, {}'.format(n_event, n_censored, n_good_traces)
-            plt.text(0.6, 0.8, k_str, transform=ax.transAxes, fontsize=14)
-            plt.text(0.6, 0.6, string, transform=ax.transAxes, fontsize=14)
-            plt.xlim((0, max_time))
+            stat_counts = (n_event, n_censored, n_good_traces)
+            save_fig_path = datapath / (i_sheet + '_' + item + '_dwell' + '.' + im_format)
+            plot_survival_curve(kmf, exf, i, stat_counts, save_fig_path,
+                                x_right_lim=max_time)
 
-            save_fig_path = datapath / (i_sheet+'_' + item + '_dwell' + '.' + im_format)
-            # plt.show()
-            plt.rcParams['svg.fonttype'] = 'none'
-            plt.savefig(save_fig_path, format=im_format, Transparent=True,
-                        dpi=300, bbox_inches='tight')
-            plt.close()
+
+def plot_survival_curve(kmf: KaplanMeierFitter,
+                        exf: ExponentialFitter,
+                        state: int,
+                        stat_counts: Tuple[int, int, int],
+                        save_path: Path,
+                        x_right_lim: float = None):
+    on_off_str = ['on', 'off']
+    obs_off_str = ['obs', 'off']
+    ax = kmf.plot()
+    exf.plot_survival_function(ax=ax, ci_show=False)
+    ax.get_legend().remove()
+    plt.xlabel(r'$\tau_{{{}}}$ (s)'.format(on_off_str[state]), fontsize=16)
+    plt.ylabel('probability', fontsize=16)
+    k_str = r'$k_{{{}}}$ = {:.1f} s'.format(obs_off_str[state], exf.lambda_)
+    string = '{}, {}, {}'.format(*stat_counts)
+    plt.text(0.6, 0.8, k_str, transform=ax.transAxes, fontsize=14)
+    plt.text(0.6, 0.6, string, transform=ax.transAxes, fontsize=14)
+    plt.xlim(left=0)
+    if x_right_lim is not None:
+        plt.xlim(right=x_right_lim)
+
+    plt.rcParams['svg.fonttype'] = 'none'
+    plt.savefig(save_path, format='svg', Transparent=True,
+                dpi=300, bbox_inches='tight')
+    plt.close()
 
 
 def main():
